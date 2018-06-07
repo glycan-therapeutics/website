280a6361b7614aad722a2858f993e801b0a8e9d4
aliu4 <aliu4@uncc.edu>
2018-06-07 11:39:28 -0400

    registration and login forms

--- glycanapi.php
+++ glycanapi.php
@@ -1,13 +1,16 @@
 <?php
-$server = "localhost";
-$user = "kliu10";
-$pass = "Saweqr1!";
-$dbname = "glycan";
+if(!isset($connection)) {
+	$config = parse_ini_file('../dbconfig/config.ini');
+	$server = $config['server'];
+	$user = $config['user'];
+	$pass = $config['pass'];
+	$dbname = $config['dbname'];
 
-$conn = new mysqli($server, $user, $pass, $dbname);
+	$conn = new mysqli($server, $user, $pass, $dbname);
 
-if($conn->connect_error) {
-	die("Connection failed: ").$conn->connect_error;
+	if($conn->connect_error) {
+		die("Connection failed: ").$conn->connect_error;
+	}
 }
 
 $method = $_SERVER['REQUEST_METHOD'];
@@ -18,7 +21,7 @@ $input = json_decode($postdata);
 //to access table
 $table = preg_replace('/[^a-z0-9_]+/i','',array_shift($request));
 //specific area of table
-$key = array_shift($request)+0;
+$key = array_shift($request);
 $options = [
 	'cost' => 11,
 ];
@@ -35,21 +38,49 @@ switch($method) {
 		else if($table == 'login'){
 			$ip=$_GET['ip'];
 			$email=$_GET['email'];
-			$query="SELECT login.date, login.login_successful, users.FirstName, users.LastName , users.id FROM login INNER JOIN users ON login.email= users.email WHERE date = (SELECT MAX(date) FROM login WHERE(IP = '$ip' AND email='$email'))";
+			$query=$conn->prepare("SELECT login.date, login.login_successful, users.FirstName, users.LastName , users.id FROM login INNER JOIN users ON login.email= users.email WHERE date = (SELECT MAX(date) FROM login WHERE(IP = ? AND email=?))");
+			$query->bind_param('ss',$ip, $email);
+			$query->execute();
+			$result = $query->get_result();
+			$outp = array();
+
+			while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+				array_push($outp, $rs);
+			}
+			$conn->close();
+			echo(json_encode($outp));
+			exit();
 			break;
 		}
 		else {
 			$query = "SELECT * FROM `$table` ORDER BY `$table`.id ASC";
+			$result = $conn->query($query);
+			if(!$result) {
+				http_response_code(404);
+				die($conn->error);
+			}
+			$outp = array();
+		
+			while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+				array_push($outp, $rs);
+			}
+			
+			$conn->close();
+			echo(json_encode($outp));
+			exit();
 			break;
 		}
 
 	case 'POST':
-	$function = $input->function;
-	if($function == "register"){
+	if($key == "register"){
 		$first = $input->firstName;
 		$last = $input->lastName;
  		$email = $input->email;
 		$password = $input->password;
+		$Q1 = $input->Q1;
+		$A1 = $input->A1;
+		$Q2 = $input->Q2;
+		$A2 = $input->A2;
 		$date = date("Y-m-d H:i:s");
 		$hash = password_hash($password, PASSWORD_BCRYPT, $options);		
 			$query ="INSERT INTO users(FirstName, LastName, email, password, created) VALUES (?,?,?,?,?)";	
@@ -57,6 +88,10 @@ switch($method) {
 			$result->bind_param('sssss', $first, $last, $email, $hash, $date);	
 			if ($result->execute()) {
 				echo "Email successfully registered";
+				$query ="INSERT INTO `users:recovery`(UID, Q1, Q2, A1, A2) VALUES ((SELECT id FROM users WHERE email = ?),?,?,?,?)";	
+				$result=$conn->prepare($query);
+				$result->bind_param('sssss', $email, $Q1, $Q2, $A1, $A2);	
+				$result->execute();
 			}
 			else{
 				echo "Email already exists";
@@ -64,13 +99,20 @@ switch($method) {
 		break;
 	}		
 
-	else if($function == "login-attempt"){
+	else if($key == "login-attempt"){
 		$email = $input->email;
 		$password = $input->password;
 		$ip = $input->ip;
 		$date = date("Y-m-d H:i:s");
 		$verified = 0;
-		$hashedPasswordFromDB=$conn->query("SELECT password FROM users WHERE email = '$email'")->fetch_object()->password;
+		$result=$conn->prepare("SELECT password FROM users WHERE email = ?");
+		$result->bind_param('s',$email);
+		$result->execute();
+		$result->bind_result($passwordfromDB);
+		while ($result->fetch()) {
+			$hashedPasswordFromDB=$passwordfromDB;	
+		}
+
 		if (password_verify($password, $hashedPasswordFromDB)) {
 			echo 'Password is valid!';
 			$verified = 1;
@@ -88,20 +130,8 @@ switch($method) {
 
 //prints json to be used by JS
 if($method == "GET") {
-	$result = $conn->query($query);
-	if(!$result) {
-		http_response_code(404);
-		die($conn->error);
-	}
-	$outp = array();
-
-	while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
-		array_push($outp, $rs);
-	}
-	
-	$conn->close();
-	echo(json_encode($outp));
-	exit();
+	if($table != "login"){
+}
 }
 
 //Add a user
--- js/products.js
+++ js/products.js
@@ -57,7 +57,12 @@ app.controller('init', function ($rootScope, $scope, $uibModal, $document, $http
 			$scope.compounds[x]['isCollapsed'] = true;
 		}
 	});
-
+	$scope.logout = function(){
+		console.log(localStorage.getItem("Token"));
+		console.log(localStorage.removeItem("Token"));
+		console.log(localStorage.removeItem("Name"));
+		$scope.Username=null;
+	}
 	$scope.nextSlide = function () {
 		if ($scope.active === $scope.slides.length - 1)
 			$scope.active = 0;
@@ -766,9 +771,26 @@ app.controller('loginCtrl', function ($location, $scope, $sce, $http, $uibModal,
 		.then(function (response) {
 			$scope.ip = response.data.ip;
 		});
-	$scope.name = "";
-	$scope.email = "";
-	$scope.password = "";
+	$scope.securityQ1 = [
+		"Select a security question",
+		"What is your mother's maiden name?",
+		"What is your favorite color?",
+		"What was the name of your first pet?",
+		"What was the name of your elementary / primary school?",
+		"What is your favorite food?",
+		"What city were you born in?"
+	
+	];
+	$scope.securityQ2 = [
+		"Select a security question",
+		"What is your mother's maiden name?",
+		"What is your favorite color?",
+		"What was the name of your first pet?",
+		"What was the name of your elementary / primary school?",
+		"What is your favorite food?",
+		"What city were you born in?"
+		];
+
 	$scope.register = function () {
 		var request = $http({
 			method: "POST",
@@ -778,7 +800,10 @@ app.controller('loginCtrl', function ($location, $scope, $sce, $http, $uibModal,
 				'lastName': $scope.lastName,
 				'email': $scope.email,
 				'password': $scope.password,
-				'function': 'register'
+				'Q1': $scope.Q1,
+				'Q2': $scope.Q2,
+				'A1': $scope.A1,
+				'A2': $scope.A2,
 			},
 			headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
 		}).then(function(response){
@@ -794,7 +819,6 @@ app.controller('loginCtrl', function ($location, $scope, $sce, $http, $uibModal,
 					'email': $scope.email,
 					'password': $scope.password,
 					'ip': $scope.ip,
-					'function': 'login-attempt'
 				},
 				headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
 			}).then(function(){
@@ -821,7 +845,6 @@ app.controller('loginCtrl', function ($location, $scope, $sce, $http, $uibModal,
 						headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
 					}).then(function (response) {
 						$scope.token = response.data;
-						console.log($scope.token);
 						localStorage.setItem("Name", $scope.attempt[0].FirstName+" "+$scope.attempt[0].LastName);
 						localStorage.setItem("Token", $scope.token);
 						$window.location.href = '../';
